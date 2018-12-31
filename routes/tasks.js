c3d5b8598909514def0d5ce3fd3b8752ca1a4a70
helloworldwen <helloworld.wen@gmail.com>
2018-12-31 21:42:28 +0800

    jenkins测试

--- routes/tasks.js
+++ routes/tasks.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res, next) => {
         id: 1,
         done: false,
         title: "测试任务",
-        description: "描述文案",
+        description: "描述文案,Jenkins测试",
         processor: {
             id:10,
             avator:"https://avatars0.githubusercontent.com/u/22772199?s=460&v=4",
