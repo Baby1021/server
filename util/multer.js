1eefba229f545153f420e3f1c6de3e1ebded9b8c
helloworldwen <helloworld.wen@gmail.com>
2019-01-26 22:56:19 +0800

    修复服务器不能保存文件的问题

--- util/multer.js
+++ util/multer.js
@@ -3,7 +3,8 @@ var crypto = require('crypto');
 
 var storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, 'public/images/loves')
+        // todo 配置文件
+        cb(null, '../static_server/public/images/')
     },
     filename: function(req, file, cb) {
         const filename = file.originalname;
