151520bb2c6e802409782b48ec3fa10f00a046d4
laiyuanwen <helloworld.wen@gmail.com>
2020-03-23 20:36:34 +0800

    ✨保存用户pushToken✨

--- app/router.ts
+++ app/router.ts
@@ -7,6 +7,7 @@ export default (app: Application) => {
   const { controller, router } = app;
 
   router.get('/', controller.home.index);
+  router.get('/api/v1/user/pushToken', controller.home.index);
 
   Bill(app)
   Location(app)
